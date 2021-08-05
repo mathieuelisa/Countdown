9a5079ae8d3073a199952033dfb687d110df2273
mathieuelisa <mathieu.elisa@gmail.com>
2021-08-05 18:25:08 +0200

    Adding restart feature and optimization

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,8 @@
 const app = {
   timeLeft: document.querySelector("#timeleft"),
   startButton: document.querySelector(".start"),
-  number: 5,
+  number: 10,
+  defaultChrono: 10,
 
   getStarted: () => {
     app.timeLeft.textContent = app.number;
@@ -9,15 +10,36 @@ const app = {
   actionButton: () => {
     app.startButton.addEventListener("click", app.handleButton);
   },
-  handleButton: (event) => {
+  handleButton: async (event) => {
     event.preventDefault();
+    if (app.inter !== null) {
+      clearInterval(app.inter);
+      app.inter = null;
+    }
+    if (app.number == 0) {
+      app.number = app.defaultChrono;
+    }
 
-    setInterval(() => {
-      if (app.number <= 0) {
-        clearInterval((app.number = 0));
+    try {
+      const endTimer = await app.timer();
+      if (endTimer) {
+        app.number = 0;
+        app.timeLeft.textContent = "fini";
+        clearInterval(app.inter);
+        app.inter = null;
       }
-      app.timeLeft.textContent = app.number--;
-    }, 1000);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  inter: null,
+  timer: () => {
+    return new Promise((resolve, reject) => {
+      app.inter = setInterval(() => {
+        if (app.number <= 0) resolve(true);
+        app.timeLeft.textContent = app.number--;
+      }, 1000);
+    });
   },
   init: () => {
     app.getStarted();
